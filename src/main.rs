6a6e2f59c8f8d1145b2afb3f08ceae4bcfd8e048
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2023-10-18 13:29:53 +0200

    drastically un-indented main

--- src/main.rs
+++ src/main.rs
@@ -20,91 +20,83 @@ fn main() {
     }
     let mut parse_result =
         nom::combinator::all_consuming(parse::wsr(parse::parts_and_statements))(&source);
-    match &mut parse_result {
-        Err(e) => println!("parse error: {:#?}", e),
-        Ok((_, parts)) => {
-            if config.test("ast1") {
-                println!("ast before preprocessing: {:#?}", parts);
-            }
-            preprocessing::normalize_domain_id_formatting(parts, config.test("local"));
-            let eq_classes = EqClasses::new(parts);
-            if config.test("eq") {
-                println!(
-                    "domain equivalence class representatives {:?}",
-                    eq_classes.get_representatives()
-                );
-                println!(
-                    "domain equivalence class representative members {:?}",
-                    eq_classes.get_representative_members()
-                );
-            }
-            eq_classes.normalize_equal_domain_ids(parts);
-            if let Err(e) = eq_classes.check_primitives() {
-                println!("domain equivalence class error: {:?}", e);
-            } else {
-                preprocessing::deanonymize_variables(parts);
-                if config.test("save") {
-                    preprocessing::add_antecedent_variables_as_pos_literals(parts);
-                }
-                if config.test("ast2") {
-                    println!("ast after preprocessing: {:#?}", parts);
-                }
-                let part_map_result = statics::PartMap::new(parts.iter());
-                match part_map_result {
-                    Err(clashing_name) => println!("clashing part name: {:?}", clashing_name),
-                    Ok(part_map) => {
-                        let dumn = part_map.depended_undefined_names().collect::<HashSet<_>>();
-                        if !dumn.is_empty() {
-                            println!("~ ~ WARNING: dependend undefined parts: {:?} ~ ~", dumn);
-                        }
-                        let ep = ExecutableProgram::new(&part_map, config.executable_config());
-                        if config.test("ir") {
-                            println!("internal representation: {:#?}", ep);
-                        }
-                        match ep {
-                            Err(e) => {
-                                println!("~ ~ ERROR: error constructing executable: {:#?}", e)
-                            }
-                            Ok(ep) => {
-                                if let Some(cycle) = ep.unbounded_domain_cycle() {
-                                    return println!("~ ~ ERROR: termination uncertain due to unbounded domain cycle: {:?} ~ ~", cycle);
-                                }
-                                if !ep.used_undeclared.is_empty() {
-                                    println!(
-                                        "~ ~ WARNING: domains undeclared but are variables or have arguements: {:?} ~ ~",
-                                        ep.used_undeclared
-                                    );
-                                }
-                                let pug = part_map.part_usage_graph();
-                                let seal_breaks = pug.iter_breaks(&ep).collect::<HashSet<_>>();
-                                if !seal_breaks.is_empty() {
-                                    println!("~ ~ WARNING: seal breaks: {:#?} ~ ~", seal_breaks);
-                                }
-                                if config.test("asp") {
-                                    println!("asp print:\n{}", ep.asp_print());
-                                }
-                                if config.test("dot") {
-                                    println!("ontology dot:\n{}", ep.ontology_dot());
-                                }
-                                let denotation_res = ep.denotation();
-                                if config.test("how") {
-                                    println!("how: {:#?}", ep.how(&denotation_res));
-                                }
-                                if !config.test("no-deno") {
-                                    if config.test("cluster") {
-                                        println!("denotation: {:#?}", denotation_res.denotation)
-                                    } else {
-                                        println!(
-                                            "denotation: {:#?}",
-                                            denotation_res.denotation.bare()
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+    let parts = match &mut parse_result {
+        Err(e) => return println!("parse error: {:#?}", e),
+        Ok((_, parts)) => parts,
+    };
+    if config.test("ast1") {
+        println!("ast before preprocessing: {:#?}", parts);
+    }
+    preprocessing::normalize_domain_id_formatting(parts, config.test("local"));
+    let eq_classes = EqClasses::new(parts);
+    if config.test("eq") {
+        println!("domain equivalence class representatives {:?}", eq_classes.get_representatives());
+        println!(
+            "domain equivalence class representative members {:?}",
+            eq_classes.get_representative_members()
+        );
+    }
+    eq_classes.normalize_equal_domain_ids(parts);
+    if let Err(e) = eq_classes.check_primitives() {
+        return println!("domain equivalence class error: {:?}", e);
+    }
+
+    preprocessing::deanonymize_variables(parts);
+    if config.test("save") {
+        preprocessing::add_antecedent_variables_as_pos_literals(parts);
+    }
+    if config.test("ast2") {
+        println!("ast after preprocessing: {:#?}", parts);
+    }
+    let part_map_result = statics::PartMap::new(parts.iter());
+    let part_map = match part_map_result {
+        Err(clashing_name) => return println!("clashing part name: {:?}", clashing_name),
+        Ok(part_map) => part_map,
+    };
+    let dumn = part_map.depended_undefined_names().collect::<HashSet<_>>();
+    if !dumn.is_empty() {
+        println!("~ ~ WARNING: dependend undefined parts: {:?} ~ ~", dumn);
+    }
+    let ep = ExecutableProgram::new(&part_map, config.executable_config());
+    if config.test("ir") {
+        println!("internal representation: {:#?}", ep);
+    }
+    let ep = match ep {
+        Err(e) => return println!("~ ~ ERROR: error constructing executable: {:#?}", e),
+        Ok(ep) => ep,
+    };
+    if let Some(cycle) = ep.unbounded_domain_cycle() {
+        return println!(
+            "~ ~ ERROR: termination uncertain due to unbounded domain cycle: {:?} ~ ~",
+            cycle
+        );
+    }
+    if !ep.used_undeclared.is_empty() {
+        println!(
+            "~ ~ WARNING: domains undeclared but are variables or have arguements: {:?} ~ ~",
+            ep.used_undeclared
+        );
+    }
+    let pug = part_map.part_usage_graph();
+    let seal_breaks = pug.iter_breaks(&ep).collect::<HashSet<_>>();
+    if !seal_breaks.is_empty() {
+        println!("~ ~ WARNING: seal breaks: {:#?} ~ ~", seal_breaks);
+    }
+    if config.test("asp") {
+        println!("asp print:\n{}", ep.asp_print());
+    }
+    if config.test("dot") {
+        println!("ontology dot:\n{}", ep.ontology_dot());
+    }
+    let denotation_res = ep.denotation();
+    if config.test("how") {
+        println!("how: {:#?}", ep.how(&denotation_res));
+    }
+    if !config.test("no-deno") {
+        if config.test("cluster") {
+            println!("denotation: {:#?}", denotation_res.denotation)
+        } else {
+            println!("denotation: {:#?}", denotation_res.denotation.bare())
         }
     }
 }
